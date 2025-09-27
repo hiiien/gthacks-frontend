45f9a9ca744e0744f6a874afc05f43ebf91ec426
diodzi <me@diodzi.dev>
2025-09-27 02:03:45 -0400

    native tabs yay !!! liquid glass !!!

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,25 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs'
+import 'react-native-reanimated'
 
 export const unstable_settings = {
   anchor: '(tabs)',
-};
+}
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    <NativeTabs>
+      <NativeTabs.Trigger name='index'>
+        <Label>Home</Label>
+        <Icon sf='house.fill' drawable='custom_android_drawable' />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name='explore'>
+        <Label>Explore</Label>
+        <Icon sf='magnifyingglass' drawable='custom_android_drawable' />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name='profile'>
+        <Icon sf='gear' drawable='custom_settings_drawable' />
+        <Label>Profile</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  )
 }
