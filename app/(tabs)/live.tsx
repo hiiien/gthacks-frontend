911bf074e57fa8bd5a9eca6fe4e26beb01913c5e
diodzi <me@diodzi.dev>
2025-09-27 23:55:04 -0400

    styled live feed wooooo (pull from backend main too)

--- app/(tabs)/live.tsx
+++ app/(tabs)/live.tsx
@@ -1,21 +1,16 @@
-import { Image } from 'expo-image'
 import { Href, useRouter } from 'expo-router' // Import Href
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import {
   ActivityIndicator,
   FlatList,
   Pressable,
-  RefreshControl,
-  StyleSheet,
   Text,
   View,
 } from 'react-native'
 
 import '../../global.css'
 
-import ParallaxScrollView from '@/components/parallax-scroll-view'
 import { IconSymbol } from '@/components/ui/icon-symbol'
-import { useThemeColor } from '@/hooks/use-theme-color'
 
 type FilterType = 'hot' | 'following'
 
@@ -23,6 +18,7 @@ type Stream = {
   room: {
     id: number
     title: string
+    gameTitle: string
     viewCount: number
   }
   owner: {
@@ -33,7 +29,15 @@ type Stream = {
   }
 }
 
-const StreamCard = ({ item }: { item: Stream }) => {
+const StreamCard = ({
+  item,
+  index,
+  streamsLength,
+}: {
+  item: Stream
+  index: number
+  streamsLength: number
+}) => {
   const router = useRouter()
   const handlePress = () => {
     router.push(`/stream/${item.room.id}` as Href)
@@ -43,28 +47,33 @@ const StreamCard = ({ item }: { item: Stream }) => {
     <Pressable onPress={handlePress}>
       {({ pressed }) => (
         <View
-          className='rounded-2xl overflow-hidden bg-neutral-100 dark:bg-neutral-800/50 border border-neutral-200 dark:border-neutral-700 shadow-sm'
-          style={{ opacity: pressed ? 0.7 : 1 }}
+          className={`mb-6 pb-6 px-4 border-b-[1px] border-zinc-900 flex flex-col gap-y-6 ${index === 0 ? 'pt-8' : ''} ${index === streamsLength - 1 ? 'pb-64' : ''}`}
         >
-          <View className='w-full aspect-video relative'>
-            <View className='absolute top-3 left-3 bg-red-400 px-2 py-1 rounded-md'>
-              <Text className='text-white text-xs font-bold tracking-widest'>
-                REP: {item.owner.repPoints.toLocaleString()}
-              </Text>
-            </View>
+          <View className='flex-grow flex items-center'>
+            <Text className='text-zinc-100 text-3xl font-medium tracking-tight'>
+              {item.room.title}
+            </Text>
+            <Text className='text-zinc-500 text-xl font-medium tracking-wide'>
+              {item.room.gameTitle}
+            </Text>
           </View>
-          <View className='p-4'>
-            <Text className='font-semibold'>{item.room.title}</Text>
-            <View className='flex-row justify-between items-center mt-2'>
-              <Text className='text-sm text-neutral-500 dark:text-neutral-400'>
-                {item.owner.username}
-              </Text>
-              <View className='flex-row items-center gap-1.5'>
-                <IconSymbol name='eye.fill' size={12} color='#808080' />
-                <Text className='text-sm text-neutral-500 dark:text-neutral-400 font-semibold'>
-                  {item.room.viewCount}
+          <View className='flex flex-row items-center justify-between'>
+            <View className='flex flex-row gap-x-3 items-center'>
+              <View className='w-12 h-12 bg-yellow-500 rounded-xl'></View>
+              <View>
+                <Text className='text-zinc-100 text-xl font-medium'>
+                  @{item.owner.username}
+                </Text>
+                <Text className='text-zinc-500'>
+                  {item.owner.repPoints} Rep
                 </Text>
               </View>
+            </View>
+            <View className='flex flex-row items-center gap-x-3'>
+              <Text className='text-zinc-100 text-lg font-medium'>
+                {item.room.viewCount}
+              </Text>
+              <IconSymbol name='eye.fill' size={18} color='white' />
             </View>
           </View>
         </View>
@@ -74,18 +83,8 @@ const StreamCard = ({ item }: { item: Stream }) => {
 }
 
 export default function LiveScreen() {
-  const [activeFilter, setActiveFilter] = useState<FilterType>('hot')
   const [streams, setStreams] = useState<Stream[]>([])
   const [isLoading, setIsLoading] = useState(true)
-  const [isRefreshing, setIsRefreshing] = useState(false)
-  const primaryColor = useThemeColor(
-    { light: '#3B82F6', dark: '#3B82F6' },
-    'text',
-  )
-  const hotColor = useThemeColor(
-    { light: '#ce1313ff', dark: '#df5317ff' },
-    'text',
-  ) // Red-500
 
   const fetchStreams = useCallback(async (filter: FilterType) => {
     setIsLoading(true)
@@ -111,103 +110,38 @@ export default function LiveScreen() {
   }, [])
 
   useEffect(() => {
-    fetchStreams(activeFilter)
-  }, [activeFilter, fetchStreams])
+    fetchStreams('following')
+  }, [fetchStreams])
 
-  const onRefresh = useCallback(async () => {
-    setIsRefreshing(true)
-    await fetchStreams(activeFilter)
-    setIsRefreshing(false)
-  }, [activeFilter, fetchStreams])
-
-  const renderContent = () => {
-    if (isLoading) {
-      return (
-        <View className='flex-1 items-center justify-center pt-20'>
-          <ActivityIndicator size='large' color={primaryColor} />
-        </View>
-      )
-    }
-
+  if (isLoading) {
     return (
-      <View className='gap-y-6'>
-        <View className='flex-row gap-3'>
-          <Pressable
-            onPress={() => setActiveFilter('hot')}
-            className={`flex-row items-center gap-2 px-4 py-2 rounded-full ${activeFilter === 'hot' ? 'bg-red-500' : 'bg-neutral-200 dark:bg-neutral-800'}`}
-          >
-            <IconSymbol
-              name='flame.fill'
-              size={14}
-              color={activeFilter === 'hot' ? '#FFFFFF' : hotColor}
-            />
-            <Text
-              className={`${activeFilter === 'hot' ? 'text-white' : 'text-neutral-600 dark:text-neutral-400'} font-bold text-sm`}
-            >
-              Hot
-            </Text>
-          </Pressable>
-          <Pressable
-            onPress={() => setActiveFilter('following')}
-            className={`flex-row items-center gap-2 px-4 py-2 rounded-full ${activeFilter === 'following' ? 'bg-blue-500' : 'bg-neutral-200 dark:bg-neutral-800'}`}
-          >
-            <IconSymbol
-              name='heart.fill'
-              size={14}
-              color={activeFilter === 'following' ? '#FFFFFF' : '#808080'}
-            />
-            <Text
-              className={`${activeFilter === 'following' ? 'text-white' : 'text-neutral-600 dark:text-neutral-400'} font-bold text-sm`}
-            >
-              Following
-            </Text>
-          </Pressable>
-        </View>
-
-        <FlatList
-          data={streams}
-          renderItem={({ item }) => <StreamCard item={item} />}
-          keyExtractor={(item) => `room-${item.room.id}`}
-          scrollEnabled={false}
-          ItemSeparatorComponent={() => <View className='h-4' />}
-          ListEmptyComponent={
-            <View className='mt-5 items-center justify-center p-5 rounded-lg bg-neutral-100 dark:bg-neutral-800'>
-              <Text>No live streams found for this filter.</Text>
-            </View>
-          }
-        />
+      <View className='flex-1 items-center justify-center pt-20'>
+        <ActivityIndicator size='large' color={'gold'} />
       </View>
     )
   }
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#FFFFFF', dark: '#1A1A1A' }}
-      headerImage={
-        <View style={styles.headerContainer}>
-          <Text className='text-zinc-50 text-3xl font-semibold'>
-            Live Streams
-          </Text>
-        </View>
-      }
-      refreshControl={
-        <RefreshControl
-          refreshing={isRefreshing}
-          onRefresh={onRefresh}
-          tintColor={primaryColor}
+    <View className='h-screen bg-zinc-950 flex flex-col'>
+      <View className='pt-24 pb-8 px-8 border-b-[1px] border-zinc-900 flex flex-row items-center gap-x-4'>
+        <View className='w-3 h-3 bg-red-500 rounded-xl'></View>
+        <Text className='text-4xl font-extrabold text-zinc-100 tracking-wide'>
+          Live Streams
+        </Text>
+      </View>
+      <View className='bg-zinc-950 flex-grow'>
+        <FlatList
+          data={streams}
+          keyExtractor={(item) => `room-${item.room.id}`}
+          renderItem={({ item, index }) => (
+            <StreamCard
+              item={item}
+              index={index}
+              streamsLength={streams.length}
+            />
+          )}
         />
-      }
-    >
-      {renderContent()}
-    </ParallaxScrollView>
+      </View>
+    </View>
   )
 }
-
-const styles = StyleSheet.create({
-  headerContainer: {
-    height: 150,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-})
